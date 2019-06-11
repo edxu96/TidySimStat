e6d6e666654cfaa69b8783dfec544feb09d80eab
edxu96 <edxu96@outlook.com>
2019-06-11 13:34:26 +0200

    initialize exercise 4

---
+++ exercise4/sim.m
@@ -0,0 +1,7 @@
+
+function [ b ] = sim( offeredTraffic, n )
+% Function to simulate the block system
+%     :param offeredTraffic: offered traffic = lambda / mu
+%     :param n: number of servers
+
+end  % function
