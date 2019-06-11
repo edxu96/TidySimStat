fee08ad2351bfdcacd4bcb0847dab4f7306628d6
edxu96 <edxu96@outlook.com>
2019-06-11 15:41:23 +0200

    update

--- exercise4/setup.m
+++ exercise4/setup.m
@@ -7,11 +7,11 @@
 % pwd
 addpath("~/Documents/GitHub/StochasticSim/exercise4")
 % ######################################################################################################################
-mu = 8;  % 1 / 64;
-matProb = ones(10, 10);
-for t = 1:10
-    for n = 1:10
-        matProb(t, n) = processPoisson(t, mu, n);
-    end
-end
-plotPdf( [1:1:10], matProb, '1.png', "PDF of N(t) from Poisson Process" );
+% mu = 8;  % 1 / 64;
+% matProb = ones(10, 10);
+% for t = 1:10
+%     for n = 1:10
+%         matProb(t, n) = processPoisson(t, mu, n);
+%     end
+% end
+% plotPdf( [1:1:10], matProb, '1.png', "PDF of N(t) from Poisson Process" );
--- exercise4/startSim.m
+++ exercise4/startSim.m
@@ -1,17 +1,30 @@
 
-function [ b ] = startSim( offeredTraffic, nServer, nCustomer )
+function [ b ] = startSim( offeredTraffic, nServer, nCustomer, lambda, mu )
 % Function to simulate the block system
 %     :param offeredTraffic: offered traffic = lambda / mu
 %     :param n: number of servers
-    exppdf(vecXstd, 1 / lambda);
+    vecTimeDepart = ones(nServer, 1)
     nCustomerBlock = 0;
     clockSim = 0;
-    nCustomerServe = 0
-    for i = 1:nCustomer
-        nCustomerServe = nCustomerServe + 1
-        clockSim = clockSim + exppdf(lambda)
-        if nCustomerServe > nServer
+    nCustomerServe = 0;
+    while i = 1:nCustomer
+        % Next customer arrive
+        clockSim = clockSim + exprnd(mu);
+        nCustomerServe = nCustomerServe + 1;
+        % Check the departed customers
+        nCustomerDepart = 0;
+        for j = 1:nServe
+            if clockSim >= vecTimeDepart(j)
+                vecTimeDepart(j) == 1;
+                nCustomerDepart = nCustomerDepart + 1;
+            end
+        end
+        % Check if blocked
+        if nCustomerServe - nCustomerDepart > nServer
             nCustomerBlock = nCustomerBlock + 1;
+            for j = 1:nServer
+                vecTimeDepart(j) = vecTimeDepart(j) + exprnd(lambda);
+            end
         end
     end
     b = nCustomerBlock / nCustomer;
