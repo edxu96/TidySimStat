0278cf01e6093eaa7fe5488eab43463e0fdec1ad
edxu96 <edxu96@outlook.com>
2019-06-11 15:56:33 +0200

    Update startSim.m

--- exercise4/startSim.m
+++ exercise4/startSim.m
@@ -3,7 +3,7 @@ function [ b ] = startSim( offeredTraffic, nServer, nCustomer, lambda, mu )
 % Function to simulate the block system
 %     :param offeredTraffic: offered traffic = lambda / mu
 %     :param n: number of servers
-    vecTimeDepart = ones(nServer, 1)
+    vecTimeDepart = ones(nServer, 1);
     nCustomerBlock = 0;
     clockSim = 0;
     nCustomerServe = 0;
@@ -12,19 +12,21 @@ function [ b ] = startSim( offeredTraffic, nServer, nCustomer, lambda, mu )
         clockSim = clockSim + exprnd(mu);
         nCustomerServe = nCustomerServe + 1;
         % Check the departed customers
-        nCustomerDepart = 0;
         for j = 1:nServe
             if clockSim >= vecTimeDepart(j)
-                vecTimeDepart(j) == 1;
-                nCustomerDepart = nCustomerDepart + 1;
+                vecTimeDepart(j) == clockSim;
+                nCustomerServe = nCustomerServe - 1;
             end
         end
         % Check if blocked
-        if nCustomerServe - nCustomerDepart > nServer
-            nCustomerBlock = nCustomerBlock + 1;
-            for j = 1:nServer
-                vecTimeDepart(j) = vecTimeDepart(j) + exprnd(lambda);
+        if nCustomerServe > nServer
+            nCustomerBlock = nCustomerBlock + (nCustomerServe - nServer);
+        else
+            j = 1;
+            while vecTimeDepart(j) != clockSim
+                j = j + 1;
             end
+            vecTimeDepart(j) = clockSim + exprnd(lambda);
         end
     end
     b = nCustomerBlock / nCustomer;
