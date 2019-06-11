5008a1de89451dc56ba2543b2156444d699c70e0
edxu96 <edxu96@outlook.com>
2019-06-11 14:35:51 +0200

    update

--- exercise4/processPoisson.m
+++ exercise4/processPoisson.m
@@ -1,8 +1,8 @@
-function [ p ] = function( t, lambda, n )
+function [ prob ] = processPoisson( t, lambda, n )
 % Calculate the probability of N(t) equals n
 %
 % :param t: time of the process
 % :param lambda: parameters in the Poisson process
 % :param n: input x
-    p = (lambda * t)^n / factorial(n) * exp(- lambda * t)
+    prob = (lambda * t)^n / factorial(n) * exp(- lambda * t);
 end  % function
--- exercise4/setup.m
+++ exercise4/setup.m
@@ -2,7 +2,16 @@
 % Author: Edward J. Xu
 % Date: 190611
 % Version: 1.0
-vecProb = ones(100, 1)
-for n = 1:100
-    vecProb[n] = processPoisson(1, 1 / 64, n)
+% ######################################################################################################################
+% cd ~/Documents/GitHub/StochasticSim/exercise4
+% pwd
+addpath("~/Documents/GitHub/StochasticSim/exercise4")
+% ######################################################################################################################
+mu = 8;  % 1 / 64;
+matProb = ones(10, 10);
+for t = 1:10
+    for n = 1:10
+        matProb(t, n) = processPoisson(t, mu, n);
+    end
 end
+plotPdf( [1:1:10], matProb, '1.png', "PDF of N(t) from Poisson Process" );
