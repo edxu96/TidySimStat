560d7bb4401046588558c655a20b284d9ac3606e
edxu96 <edxu96@outlook.com>
2019-06-12 11:27:48 +0200

    update

--- exercise5/setup.m
+++ exercise5/setup.m
@@ -7,6 +7,18 @@
 % pwd
 addpath("~/Documents/GitHub/StochasticSim/exercise5")
 % ######################################################################################################################
+whiFunc = "antithetic";
+fprintf("Method: %s.\n", whiFunc);
+if whiFunc == "exp"
+    funcSim = @exp;
+elseif whiFunc == "antithetic"
+    funcSim = @(u) (exp(u) + exp(1-u)) / 2;
+end
+nSim = 100;
 nSample = 100;
-funcSim = @exp;
-[result] = simIntegral(nSample, funcSim)
+vecResultBar = zeros(nSim, 1);
+for i = 1:nSim
+    [vecResultBar(i)] = simIntegral(nSample, funcSim);
+end
+fprintf("Mean = %f.\n", mean(vecResultBar));
+fprintf("Var = %f.\n", var(vecResultBar));
--- exercise5/simIntegral.m
+++ exercise5/simIntegral.m
@@ -1,8 +1,8 @@
-function [result] = simIntegral(nSample, funcSim)
+function [resultBar] = simIntegral(nSample, funcSim)
     vecResult = zeros(nSample, 1);
     vecSample = rand(nSample);
     for i = 1:nSample
         vecResult(i) = funcSim(vecSample(i));
     end
-    result = sum(vecResult) / nSample;
+    resultBar = sum(vecResult) / nSample;
 end
