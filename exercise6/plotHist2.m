1d69e9436655f2f7c0fd564ec15cb5aafba8bc25
edxu96 <edxu96@outlook.com>
2019-06-14 15:11:54 +0200

    update

--- exercise6/plotHist2.m
+++ exercise6/plotHist2.m
@@ -1,10 +1,15 @@
-function [ fig ] = plotHist2(vecX1, vecX2, mat, m, strFileName)
+function [ fig ] = plotHist2(vecX1, vecX2, mat, m, strFileName, whiMethod)
     % Plot the histogram of RNG result and return the vector of values
     fig = figure("Visible", "off");
     fig = histogram2(vecX1, vecX2, [-0.5:1:10.5], [-0.5:1:10.5], 'FaceColor', 'flat');
     hold on
     fig = stem3([0:m], [0:m], mat);
     hold off
-    saveas(fig, [pwd '/images/' strFileName '.png']);
-    saveas(fig, [pwd '/images/' strFileName '.fig']);
+    if whiMethod == 1
+        saveas(fig, [pwd '/images/direct' strFileName '.png']);
+        saveas(fig, [pwd '/images/direct' strFileName '.fig']);
+    elseif whiMethod == 2
+        saveas(fig, [pwd '/images/gibbs' strFileName '.png']);
+        saveas(fig, [pwd '/images/gibbs' strFileName '.fig']);
+    end
 end
--- exercise6/setup.m
+++ exercise6/setup.m
@@ -5,21 +5,21 @@
 % cd ~/Documents/GitHub/StochasticSim/exercise6
 % pwd
 addpath("~/Documents/GitHub/StochasticSim/exercise6")  % ###############################################################
-% m = 10;
-% nSample = 20000;
-% lambda = 8;
-% mu = 1;
-% aCap = lambda / mu;
-% [vecState, sState] = simRandWalkHastingsMetropolis(m, nSample, aCap);
-% save([pwd '/outputs/vecState_2.mat'], 'vecState');
-% % Calculate the Analytical Values --------------------------------------------------------------------------------------
-% vecResult = zeros(m + 1, 1);
-% for j = 0:m
-%     vecResult(j + 1) = calCount(j, aCap);
-% end
-% vecResult = vecResult / sum(vecResult);
-% % Plot the Histogram of the Result -------------------------------------------------------------------------------------
-% vecProbClass = plotHist(vecState(1000:end), [0:1:m], vecResult, m + 1, '3.png');
+m = 10;
+nSample = 20000;
+lambda = 8;
+mu = 1;
+aCap = lambda / mu;
+[vecState, sState] = simRandWalkHastingsMetropolis(m, nSample, aCap);
+save([pwd '/outputs/vecState_2.mat'], 'vecState');
+% Calculate the Analytical Values --------------------------------------------------------------------------------------
+vecResult = zeros(m + 1, 1);
+for j = 0:m
+    vecResult(j + 1) = calCount(j, aCap);
+end
+vecResult = vecResult / sum(vecResult);
+% Plot the Histogram of the Result -------------------------------------------------------------------------------------
+vecProbClass = plotHist(vecState(1000:end), [0:1:m], vecResult, m + 1, '1.png');
 % ######################################################################################################################
 clear
 clc
@@ -29,19 +29,19 @@ nRow = 6;
 nSample = 100000;
 aCap_1 = 4;
 aCap_2 = 4;
-% whiMethod = 1;
-whiMethod = 2;
+whiMethod = 1;
+% whiMethod = 2;
 [sState2] = simDistDiscrete2(m, nRow, nSample, aCap_1, aCap_2, whiMethod);
 save([pwd '/outputs/sState2_2.mat'], 'sState2');
 % Plot the Analytical Values -------------------------------------------------------------------------------------------
 matProb = zeros(m + 1);
-for i = 1:(m + 1)
-    for j = 1:i
-        matProb(i, j) = calCount2(i - 1, j - 1, aCap_1, aCap_2);
+for i = 0:m
+    for j = 0:(m - i)
+        matProb(i + 1, j + 1) = calCount2(i, j, aCap_1, aCap_2);
     end
 end
 matProb = matProb / sum(sum(matProb));
-% plotStem3(matProb, '12', m);
+plotStem3(matProb, '3', m);
 % Plot 3-D Histogram of the Result -------------------------------------------------------------------------------------
 vecX1 = zeros(nSample, 1);
 vecX2 = zeros(nSample, 1);
@@ -51,5 +51,5 @@ for i = 1:nSample
     vecX2(i) = sState2(i).x(2);
     vecX12(i) = sum(sState2(i).x);
 end
-plotHist2(vecX1, vecX2, matProb * nSample, m, '7');
+plotHist2(vecX1, vecX2, matProb * nSample, m, '4', whiMethod);
 % openfig('images/4.fig')
