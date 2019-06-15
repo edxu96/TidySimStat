31703b839ab07458f978cc711ebe01442fcaac80
edxu96 <edxu96@outlook.com>
2019-06-15 23:04:40 +0200

    Update getCellSampleSpace2dim.m

--- functions/getCellSampleSpace2dim.m
+++ functions/getCellSampleSpace2dim.m
@@ -1,9 +1,11 @@
-function [cellSampleSpace] = getCellSampleSpace2dim(vecData1, vecData2, n1, n1, funcLogic)
+
+
+function [cellSampleSpace] = getCellSampleSpace2dim(vecData1, vecData2, n1, n2, funcLogic)
     cellSampleSpace = {};
     for i = 1:n1
         for j = 1:n2
             if funcLogic(vecData1(i), vecData2(j))
-                cellSampleSpace{end + 1} = [vecData1(i), vecData2(2)];
+                cellSampleSpace{end + 1} = [vecData1(i), vecData2(j)];
             end
         end
     end
