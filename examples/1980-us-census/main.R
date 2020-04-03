585f8b617dfc1531a0d6761498f53f5b7200477c
edxu96 <edxu96@outlook.com>
2020-04-03 11:38:41 +0200

    update JB test

--- examples/1980-us-census/main.R
+++ examples/1980-us-census/main.R
@@ -23,9 +23,9 @@ dat %>%
   ggplot(aes(educ, wage_log, group = cut_width(educ, 1))) +
   geom_boxplot()
 
-mods <- list()
-mods[[1]] <- lm(wage_log ~ educ, data = dat)
-mods[[1]] %>% summary()
+mod <- lm(wage_log ~ educ, data = dat)
+
+test_jb(mod, dat)
 
 mods[[2]] <-
   dat %>%
@@ -42,105 +42,6 @@ mods[[3]] %>% summary()
 
 anova(mods[[3]], mods[[1]])
 ( - 3877 * log(summary(mods[[1]])$sigma / summary(mods[[3]])$sigma) )
-
-#### Jarque-Bera Test ####
-
-qchisq(0.95, summary(mods[[1]])$df[1], lower.tail = TRUE, log.p = FALSE)
-qchisq(0.95, 1, lower.tail = TRUE, log.p = FALSE)
-1 - pchisq(3.841459, 1)
-
-mods[[1]] %>%
-  residuals() %>%
-  tseries::jarque.bera.test() %>%
-  glance()
-
-mods[[1]] %>%
-  residuals() %>%
-  propagate::skewness() %>%
-  {.^2 * nrow(mods[[1]]) / 6}
-
-mods[[1]] %>%
-  residuals() %>%
-  propagate::kurtosis() %>%
-  {.^2 * nrow(mods[[1]]) / 24}
-
-#### White's Test ####
-
-dat %>%
-  mutate(resi2 = mods[[1]]$residuals^2) %>%
-  {lm(resi2 ~ educ + I(educ^2), data = .)} %>%
-  {summary(.)$r.squared} %>%
-  {. / 2 / (1 - .) * (nrow(dat) - 2)} 
-
-dat %>%
-  mutate(resi2 = mods[[1]]$residuals^2) %>%
-  {lm(resi2 ~ educ + I(educ^2), data = .)} %>%
-  {summary(.)$r.squared} %>%
-  {. * nrow(dat) <= qchisq(0.95, 2, lower.tail = TRUE, log.p = FALSE)}
-
-dat %>%
-  mutate(resi2 = mods[[1]]$residuals^2) %>%
-  {lm(resi2 ~ educ + I(educ^2), data = .)} %>%
-  {summary(.)$r.squared} %>%
-  {1 - pchisq(. * nrow(dat), 2)}
-
-mods[[1]] %>%
-  bptest(~ educ + I(educ^2), data = dat)
-
-mods[[1]] %>%
-  whites.htest()
-
-#### RESET Test ####
-
-mods[[1]] %>%
-  reset()
-
-dat %>%
-  mutate(y_hat = fitted(mods[[1]])) %>%
-  {lm(wage_log ~ educ + I(y_hat^2), data = .)} %>%
-  modelEffectSizes() %>%
-  {.$Effects} %>%
-  {.[3, nrow(.)] * 3873} %>%
-  {1 - pchisq(., 1)} %>%
-  {. >= 0.05}
-
-%>%
-  {. <= qchisq(0.95, 1, lower.tail = TRUE, log.p = FALSE)}
-
-
-
-mod %>%
-  {tidy(.)$p.value[3] * 3874}
-
-
-%>%
-  {tidy(.)$p.value[3]} %>%
-  {. * (nrow(dat) - 1)} %>%
-  {1 - pchisq(. * (nrow(dat) - 1), 1)}
-    
-    
-
-dat %>%
-  mutate(y_hat = fitted(mods[[1]])) %>%
-  {lm(wage_log ~ educ + I(y_hat^2), data = .)} %>%
-  {summary(.)$r.squared} %>%
-  {1 - pchisq(. * nrow(dat), 1)}
-  
-dat %>%
-  mutate(y_hat = fitted(mods[[1]])) %>%
-  {lm(wage_log ~ educ + I(y_hat^2), data = .)} %>%
-  {summary(.)$r.squared} %>%
-  {. * nrow(dat) <= qchisq(0.95, 1, lower.tail = TRUE, log.p = FALSE)}
-
-mods[[4]] <- 
-  lm(wage_log ~ educ + I(educ^2), data = dat)
-
-dat %>%
-  mutate(y_hat = fitted(mods[[4]])) %>%
-  {lm(wage_log ~ educ + I(educ^2) + I(y_hat^2), data = .)} %>%
-  modelEffectSizes() %>%
-  {.$Effects[3, 4] * 3873} %>%
-  {. <= qchisq(0.95, 1, lower.tail = TRUE, log.p = FALSE)}
 
 #### One-Sided Test ####
 
--- examples/funcs.R
+++ examples/funcs.R
@@ -22,3 +22,113 @@ new_results <- function(model, idx){
   
   return(results)
 }
+
+
+#' Perform Jarque-Bera test
+#' @author Edward J. Xu
+#' @description If `if_accept` is true, the model is accepted.
+test_jb <- function(mod, dat, prob){
+  if(missing(prob)){prob = 0.05}
+  
+  stat_skew <- 
+    mod %>%
+    residuals() %>%
+    propagate::skewness() %>%
+    {.^2 * nrow(dat) / 6}
+  
+  stat_kurt <- 
+    mod %>%
+    residuals() %>%
+    propagate::kurtosis() %>%
+    {.^2 * nrow(dat) / 24}
+  
+  stat <- stat_skew + stat_kurt
+    
+  df1 <- 2
+  
+  p_value <- 
+    stat %>%
+    {1 - pchisq(., df1)}
+  
+  results <- tibble(
+    method = "Jarque-Bera", stat = stat, df1 = df1, df2 = nrow(dat) - df1,  
+    p_value = p_value,prob = prob, if_accept = {p_value <= prob}, 
+    if_pass = {p_value <= prob}
+  )
+  
+  return(results)
+}
+
+
+#' Perform White's test
+#' @author Edward J. Xu
+#' @description If `if_accept` is true, the null hypothesis is accepted.
+test_white <- function(mod, dat, f, df1, prob){
+  if(missing(prob)){prob = 0.05}
+  
+  dat %<>%
+    mutate(resi2 = mod$residuals^2)
+  
+  stat <-
+    lm(f, data = dat) %>%
+    {summary(.)$r.squared} %>%
+    {. * nrow(dat_2)}
+  
+  p_value <- 
+    stat %>%
+    {1 - pchisq(., df1)}
+  
+  results <- tibble(
+    method = "White", stat = stat, df1 = df1, df2 = nrow(dat) - df1,  
+    p_value = p_value,prob = prob, if_accept = {p_value <= prob}, 
+    if_pass = {p_value >= prob}
+  )
+
+  return(results)
+}
+
+
+#' Perform RESET test
+#' @author Edward J. Xu
+#' @description If `if_accept` is true, the null hypothesis is accepted.
+test_reset <- function(mod, dat, prob){
+  if(missing(prob)){prob = 0.05}
+  
+  f <- 
+    mod %>%
+    formula() %>%
+    update.formula(. ~ . + I(y_hat^2))
+  
+  mod_test <- 
+    dat %>%
+    mutate(y_hat = fitted(mod)) %>%
+    {lm(f, data = .)}
+
+  stat <-
+    mod_test %>%
+    {quiet(lmSupport::modelEffectSizes(.))} %>%
+    {.$Effects} %>%
+    {.[nrow(.), 3] * (nrow(dat) - 1)} 
+  
+  df1 <- 1
+  p_value <- 
+    stat %>%
+    {1 - pchisq(., df1)}
+
+  results <- tibble(
+    method = "RESET", stat = stat, df1 = df1, df2 = nrow(dat) - df1,  
+    p_value = p_value,prob = prob, if_accept = {p_value <= prob}, 
+    if_pass = {p_value >= prob}
+    )
+
+  return(results)
+}
+
+
+#' To mute the output of operation `x`
+#' @author Hadley Wickham
+quiet <- function(x) { 
+  sink(tempfile()) 
+  on.exit(sink()) 
+  invisible(force(x)) 
+} 
