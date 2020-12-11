f90cdbaf9fd014b9fb5fd137f87c431834dd794e
edxu96 <edxu96@outlook.com>
2020-12-11 18:53:43 +0100

    update init file

--- src/TidySimStat/__init__.py
+++ src/TidySimStat/__init__.py
@@ -17,11 +17,3 @@ __all__ = [
     "inference", "drv", "crv", "auxiliary", 
     "exam", "des_queue", "estimation", "mcmc"
 ]
-
-# if __name__ == '__main__':
-#     print("`TidySimStat.py` is a module by Edward J. Xu. Last modifed date."
-#         "is June 1, 2020.")
-# elif __name__ == 'TidySimStat':
-#     print("`TidySimStat.py` by Edward J. Xu is imported. Copyright all "
-#         "reserved. \n"
-#         "Last modifed date is June 2, 2020.")
